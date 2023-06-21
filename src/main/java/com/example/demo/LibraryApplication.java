06d159ec72eaff5d4e9c1dd64013c3d8d1128cfc
luca.serafini <luca.serafini@iongroup.com>
2023-06-22 00:23:23 +0200

    Completed refactoring

--- src/main/java/com/example/demo/LibraryApplication.java
+++ src/main/java/com/example/demo/LibraryApplication.java
@@ -3,10 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+
 @SpringBootApplication
+@Configuration
 @ComponentScan(basePackages = "com.example.*")
 @EntityScan("com.example.model")
 @EnableJpaRepositories(basePackages = "com.example.db")
@@ -15,5 +20,9 @@ public class LibraryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApplication.class, args);
 	}
-
+	
+	@Bean
+	public LayoutDialect layoutDialect() {
+	    return new LayoutDialect();
+	}
 }
