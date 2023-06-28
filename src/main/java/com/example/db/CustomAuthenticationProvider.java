ba995be8cc20fca80a121ec8bb3b3b9fff68ac01
luca.serafini <luca.serafini@iongroup.com>
2023-06-28 16:54:40 +0200

    Minor fixes

--- src/main/java/com/example/db/CustomAuthenticationProvider.java
+++ src/main/java/com/example/db/CustomAuthenticationProvider.java
@@ -1,9 +1,6 @@
 package com.example.db;
 
-import javax.annotation.Resource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
